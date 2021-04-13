7046ebedd05b91fa1b66477656e5f85768ce7407
yles9056 <yles9056@gmail.com>
2021-04-13 15:21:09 +0800

    -Use uuid for KalmanBoxTracker.id -Return index of det -Add Sort.reset() -Return deleted KalmanBoxTracker

--- sort.py
+++ sort.py
@@ -18,21 +18,10 @@
 from __future__ import print_function
 
 import os
+import uuid
 
-import matplotlib
 import numpy as np
-
-matplotlib.use("TkAgg")
-import argparse
-import glob
-import time
-
-import matplotlib.patches as patches
-import matplotlib.pyplot as plt
 from filterpy.kalman import KalmanFilter
-from skimage import io
-
-np.random.seed(0)
 
 
 def linear_assignment(cost_matrix):
@@ -107,8 +96,6 @@ class KalmanBoxTracker(object):
     This class represents the internal state of individual tracked objects observed as bbox.
     """
 
-    count = 0
-
     def __init__(self, bbox):
         """
         Initialises a tracker using initial bounding box.
@@ -145,12 +132,13 @@ class KalmanBoxTracker(object):
 
         self.kf.x[:4] = convert_bbox_to_z(bbox)
         self.time_since_update = 0
-        self.id = KalmanBoxTracker.count
-        KalmanBoxTracker.count += 1
+        self.uid = str(uuid.uuid1())  # CHANGED: use uid instead of id
         self.history = []
         self.hits = 0
         self.hit_streak = 0
         self.age = 0
+
+        self.det_idx = None  # ADDED: det_idx corresponds to index of dets
 
     def update(self, bbox):
         """
@@ -243,6 +231,10 @@ class Sort(object):
         self.trackers = []
         self.frame_count = 0
 
+    def reset(self):
+        self.trackers = []
+        self.frame_count = 0
+
     def update(self, dets=np.empty((0, 5))):
         """
         Params:
@@ -252,11 +244,19 @@ class Sort(object):
 
         NOTE: The number of objects returned may differ from the number of detections provided.
         """
-        self.frame_count += 1
+        # ADDED: prevent overflow
+        if self.frame_count <= self.min_hits:
+            self.frame_count += 1
         # get predicted locations from existing trackers.
         trks = np.zeros((len(self.trackers), 5))
         to_del = []
         ret = []
+
+        # ADDED: return more informations
+        rtn_uids = []
+        del_uids = []
+        det_idxes = []
+
         for t, trk in enumerate(trks):
             pos = self.trackers[t].predict()[0]
             trk[:] = [pos[0], pos[1], pos[2], pos[3], 0]
@@ -264,7 +264,9 @@ class Sort(object):
                 to_del.append(t)
         trks = np.ma.compress_rows(np.ma.masked_invalid(trks))
         for t in reversed(to_del):
-            self.trackers.pop(t)
+            del_tracker = self.trackers.pop(t)
+            del_uids.append(del_tracker.uid)  # ADDED: return deleted uid
+
         matched, unmatched_dets, unmatched_trks = associate_detections_to_trackers(
             dets, trks, self.iou_threshold
         )
@@ -272,10 +274,16 @@ class Sort(object):
         # update matched trackers with assigned detections
         for m in matched:
             self.trackers[m[1]].update(dets[m[0], :])
+            # ADDED: set det_idx which corresponds to index of dets
+            self.trackers[m[1]].det_idx = m[0]
+            # ADDED: prevent overflow
+            if self.trackers[m[1]].hit_streak > self.min_hits:
+                self.trackers[m[1]].hit_streak = self.min_hits
 
         # create and initialise new trackers for unmatched detections
         for i in unmatched_dets:
             trk = KalmanBoxTracker(dets[i, :])
+            trk.det_idx = i  # ADDED: set det_idx which corresponds to index of dets
             self.trackers.append(trk)
         i = len(self.trackers)
         for trk in reversed(self.trackers):
@@ -283,131 +291,19 @@ class Sort(object):
             if (trk.time_since_update < 1) and (
                 trk.hit_streak >= self.min_hits or self.frame_count <= self.min_hits
             ):
-                ret.append(
-                    np.concatenate((d, [trk.id + 1])).reshape(1, -1)
-                )  # +1 as MOT benchmark requires positive
+                ret.append(d.reshape(1, -1))  # CHANGED: ret contains boxes only
+                rtn_uids.append(trk.uid)  # ADDED: return tracked uid
+                det_idxes.append(trk.det_idx)  # ADDED: return det_idx
             i -= 1
             # remove dead tracklet
             if trk.time_since_update > self.max_age:
-                self.trackers.pop(i)
+                del_tracker = self.trackers.pop(i)
+                del_uids.append(del_tracker.uid)  # ADDED: return deleted uid
+
+        # CHANGED: ret contains boxes only
+        boxes = np.empty((0, 4))
         if len(ret) > 0:
-            return np.concatenate(ret)
-        return np.empty((0, 5))
+            boxes = np.concatenate(ret)
 
-
-def parse_args():
-    """Parse input arguments."""
-    parser = argparse.ArgumentParser(description="SORT demo")
-    parser.add_argument(
-        "--display",
-        dest="display",
-        help="Display online tracker output (slow) [False]",
-        action="store_true",
-    )
-    parser.add_argument(
-        "--seq_path", help="Path to detections.", type=str, default="data"
-    )
-    parser.add_argument(
-        "--phase", help="Subdirectory in seq_path.", type=str, default="train"
-    )
-    parser.add_argument(
-        "--max_age",
-        help="Maximum number of frames to keep alive a track without associated detections.",
-        type=int,
-        default=1,
-    )
-    parser.add_argument(
-        "--min_hits",
-        help="Minimum number of associated detections before track is initialised.",
-        type=int,
-        default=3,
-    )
-    parser.add_argument(
-        "--iou_threshold", help="Minimum IOU for match.", type=float, default=0.3
-    )
-    args = parser.parse_args()
-    return args
-
-
-if __name__ == "__main__":
-    # all train
-    args = parse_args()
-    display = args.display
-    phase = args.phase
-    total_time = 0.0
-    total_frames = 0
-    colours = np.random.rand(32, 3)  # used only for display
-    if display:
-        if not os.path.exists("mot_benchmark"):
-            print(
-                "\n\tERROR: mot_benchmark link not found!\n\n    Create a symbolic link to the MOT benchmark\n    (https://motchallenge.net/data/2D_MOT_2015/#download). E.g.:\n\n    $ ln -s /path/to/MOT2015_challenge/2DMOT2015 mot_benchmark\n\n"
-            )
-            exit()
-        plt.ion()
-        fig = plt.figure()
-        ax1 = fig.add_subplot(111, aspect="equal")
-
-    if not os.path.exists("output"):
-        os.makedirs("output")
-    pattern = os.path.join(args.seq_path, phase, "*", "det", "det.txt")
-    for seq_dets_fn in glob.glob(pattern):
-        mot_tracker = Sort(
-            max_age=args.max_age,
-            min_hits=args.min_hits,
-            iou_threshold=args.iou_threshold,
-        )  # create instance of the SORT tracker
-        seq_dets = np.loadtxt(seq_dets_fn, delimiter=",")
-        seq = seq_dets_fn[pattern.find("*") :].split(os.path.sep)[0]
-
-        with open(os.path.join("output", "%s.txt" % (seq)), "w") as out_file:
-            print("Processing %s." % (seq))
-            for frame in range(int(seq_dets[:, 0].max())):
-                frame += 1  # detection and frame numbers begin at 1
-                dets = seq_dets[seq_dets[:, 0] == frame, 2:7]
-                dets[:, 2:4] += dets[:, 0:2]  # convert to [x1,y1,w,h] to [x1,y1,x2,y2]
-                total_frames += 1
-
-                if display:
-                    fn = os.path.join(
-                        "mot_benchmark", phase, seq, "img1", "%06d.jpg" % (frame)
-                    )
-                    im = io.imread(fn)
-                    ax1.imshow(im)
-                    plt.title(seq + " Tracked Targets")
-
-                start_time = time.time()
-                trackers = mot_tracker.update(dets)
-                cycle_time = time.time() - start_time
-                total_time += cycle_time
-
-                for d in trackers:
-                    print(
-                        "%d,%d,%.2f,%.2f,%.2f,%.2f,1,-1,-1,-1"
-                        % (frame, d[4], d[0], d[1], d[2] - d[0], d[3] - d[1]),
-                        file=out_file,
-                    )
-                    if display:
-                        d = d.astype(np.int32)
-                        ax1.add_patch(
-                            patches.Rectangle(
-                                (d[0], d[1]),
-                                d[2] - d[0],
-                                d[3] - d[1],
-                                fill=False,
-                                lw=3,
-                                ec=colours[d[4] % 32, :],
-                            )
-                        )
-
-                if display:
-                    fig.canvas.flush_events()
-                    plt.draw()
-                    ax1.cla()
-
-    print(
-        "Total Tracking took: %.3f seconds for %d frames or %.1f FPS"
-        % (total_time, total_frames, total_frames / total_time)
-    )
-
-    if display:
-        print("Note: to get real runtime results run without the option: --display")
+        # CHANGED: return more informations
+        return boxes, rtn_uids, del_uids, det_idxes
